44e555e482f38bc2aefebaf9cc1920b012eee871
Manish <manishjandu1@gmail.com>
2021-09-07 15:45:41 +0530

    FragmentProfile: Internet and Error handling, tabs layout removed

--- app/src/main/java/com/scaler/microblogs/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/scaler/microblogs/ui/profile/ProfileFragment.kt
@@ -1,141 +1,138 @@
 package com.scaler.microblogs.ui.profile
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
-import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
-import com.google.android.material.tabs.TabLayout
+import com.google.android.material.snackbar.Snackbar
 import com.scaler.libconduit.models.Profile
 import com.scaler.microblogs.R
-import com.scaler.microblogs.adapters.ArticleAdapter
 import com.scaler.microblogs.databinding.FragmentProfileBinding
-import com.scaler.microblogs.utils.ArticleType
+import com.scaler.microblogs.utils.InternetConnectivity.ConnectivityManager
+import com.scaler.microblogs.utils.NetworkResult
 import com.scaler.microblogs.viewmodels.ProfileViewModel
+import com.scaler.microblogs.viewmodels.ProfileViewModel.ProfileEvent.ErrorFollowUnFollow
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class ProfileFragment : Fragment(R.layout.fragment_profile) {
-    private val profileViewModel: ProfileViewModel by viewModels()
-
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
-
-    private lateinit var userFeedAdapter: ArticleAdapter
-    private lateinit var userFavouriteFeedAdapter: ArticleAdapter
+    private val profileViewModel: ProfileViewModel by viewModels()
     private val args: ProfileFragmentArgs by navArgs()
-
-    private var isLoggedIn: Boolean = false
-    private var isFollowing: Boolean = false
     private lateinit var userName: String
+    private var isLoggedIn = false
+    private var isFollowing = false
+
+    @Inject
+    lateinit var connectivityManager: ConnectivityManager
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentProfileBinding.bind(view)
 
-        userFeedAdapter = ArticleAdapter(OnArticleClick(), ArticleType.ARTICLE)
-        userFavouriteFeedAdapter = ArticleAdapter(OnArticleClick(), ArticleType.ARTICLE)
-
         userName = args.userName
-        profileViewModel.getUser(userName)
-        profileViewModel.checkIfLoggedIn()
 
-        setupRecyclerView()
+        observeInternetConnection()
+        observeProfile()
+        observeProfileEvents()
+    }
 
-        profileViewModel.isLoggedIn.observe(viewLifecycleOwner) {
-            it?.let {
-                if (it) {
-                    isLoggedIn = true
-                    profileViewModel.getProfileFromAuthRepo(userName)
-                    currentlyLoggedIn()
-
+    private fun observeInternetConnection() {
+        connectivityManager.isNetworkAvailable.observe(viewLifecycleOwner) {
+            it?.let { isInternetAvailable ->
+                if (isInternetAvailable) {
+                    observeIfLoggedIn()
                 } else {
-                    isLoggedIn = false
-                    profileViewModel.getProfileFromRepo(userName)
-                    currentlyLoggedOut()
+                    setViewError()
                 }
             }
         }
+    }
 
+    private fun observeIfLoggedIn() {
+        profileViewModel.checkIfLoggedIn()
+        profileViewModel.isLoggedIn.observe(viewLifecycleOwner) {
+            it?.let { _isLoggedIn ->
+                setLoggedIn(_isLoggedIn)
+                setFollowUnfollowButton()
+                getProfile()
+            }
+        }
+    }
+
+    private fun setLoggedIn(isLoggedIn: Boolean) {
+        this.isLoggedIn = isLoggedIn
+    }
+
+    private fun setFollowUnfollowButton() {
+        setFollowUnfollowButtonText()
+        if (isLoggedIn) {
+            binding.buttonFollowUnfollow.setOnClickListener {
+                profileViewModel.followUnfollowAccount(userName, isFollowing)
+                toggleFollowUnfollow()
+            }
+        } else {
+            binding.buttonFollowUnfollow.setOnClickListener {
+                Toast.makeText(requireContext(), "Currently logged out.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun setFollowUnfollowButtonText() {
+        binding.buttonFollowUnfollow.text = if (isFollowing) "UnFollow" else "Follow"
+    }
+
+    private fun toggleFollowUnfollow() {
+        this.isFollowing = !isFollowing
+        setFollowUnfollowButtonText()
+    }
+
+    private fun getProfile() {
+        if (isLoggedIn) {
+            profileViewModel.getProfileFromAuthRepo(userName)
+        } else {
+            profileViewModel.getProfileFromRepo(userName)
+        }
+    }
+
+    private fun observeProfile() {
+        profileViewModel.profile.observe(viewLifecycleOwner) { it ->
+            it?.let { response ->
+                when (response) {
+                    is NetworkResult.Loading -> {
+                        setViewLoading()
+                    }
+                    is NetworkResult.Success -> {
+                        setViewProfile()
+                        setProfile(response.data!!)
+                    }
+                    is NetworkResult.Error -> {
+                        setViewError(response.message.toString())
+                    }
+                }
+            }
+        }
+    }
+
+    private fun observeProfileEvents() {
         viewLifecycleOwner.lifecycleScope.launchWhenStarted {
             profileViewModel.profileEvent.collect { event ->
                 when (event) {
-                    is ProfileViewModel.ProfileEvent.StartObserving -> {
-                        startObserving()
-                    }
-                    is ProfileViewModel.ProfileEvent.LoggedOut -> {
-                        currentlyLoggedOut()
-                    }
-                }
-            }
-        }
-
-        profileViewModel.profile.observe(viewLifecycleOwner) {
-            it?.let {
-                setProfile(it)
-            }
-        }
-
-        val tabsLayout = binding.tabsLayoutCurrentUser
-        val userFeed = tabsLayout.getTabAt(0)
-        val userFavouriteFeed = tabsLayout.getTabAt(1)
-
-        binding.tabsLayoutCurrentUser.addOnTabSelectedListener(object :
-            TabLayout.OnTabSelectedListener {
-            override fun onTabSelected(tab: TabLayout.Tab?) {
-                when (tab) {
-                    userFeed -> {
-                        binding.recyclerViewUserFavouritesFeed.visibility = View.GONE
-                        binding.recyclerViewUserFeed.visibility = View.VISIBLE
-                    }
-                    userFavouriteFeed -> {
-                        binding.recyclerViewUserFavouritesFeed.visibility = View.VISIBLE
-                        binding.recyclerViewUserFeed.visibility = View.GONE
+                    //error while follow unfollow operation.
+                    //toggle follow unfollow
+                    is ErrorFollowUnFollow -> {
+                        toggleFollowUnfollow()
+                        showSnackBar(event.errorMessage)
                     }
                 }
             }
-
-            override fun onTabUnselected(tab: TabLayout.Tab?) {
-                when (tab) {
-                    userFeed -> {
-                    }
-                    userFavouriteFeed -> {
-                    }
-                }
-            }
-
-            override fun onTabReselected(tab: TabLayout.Tab?) {
-                when (tab) {
-                    userFeed -> {
-                    }
-                    userFavouriteFeed -> {
-                    }
-                }
-            }
-
-        })
-
-    }
-
-    private fun currentlyLoggedIn() {
-        binding.buttonFollowUnfollow.text = "Follow"
-        binding.buttonFollowUnfollow.setOnClickListener {
-            profileViewModel.followUnfollowAccount(userName,isFollowing)
-        }
-    }
-
-    @SuppressLint("SetTextI18n")
-    private fun currentlyLoggedOut() {
-        binding.buttonFollowUnfollow.text = "Follow"
-        binding.buttonFollowUnfollow.setOnClickListener {
-            Toast.makeText(requireContext(), "Currently Logged out", Toast.LENGTH_SHORT).show()
         }
     }
 
@@ -143,14 +140,9 @@ class ProfileFragment : Fragment(R.layout.fragment_profile) {
         binding.apply {
             textViewUserName.text = profile.username
             textViewUserBio.text = profile.bio ?: ""
-            if (isLoggedIn) {
-                if (profile.following!!) {
-                    isFollowing = true
-                    binding.buttonFollowUnfollow.text = "UnFollow"
-                } else {
-                    isFollowing=false
-                    binding.buttonFollowUnfollow.text = "Follow"
-                }
+            profile.following?.let { following ->
+                setIsFollowing(following)
+                setFollowUnfollowButtonText()
             }
             Glide.with(imageViewProfile)
                 .load(profile.image)
@@ -159,41 +151,36 @@ class ProfileFragment : Fragment(R.layout.fragment_profile) {
         }
     }
 
-    private fun setupRecyclerView() {
+    private fun setIsFollowing(following: Boolean) {
+        this.isFollowing = following
+    }
+
+    private fun showSnackBar(message: String) {
+        Snackbar.make(requireView(), message, Snackbar.LENGTH_SHORT).show()
+    }
+
+    private fun setViewError(message: String = "No Internet Connection.") {
         binding.apply {
-            recyclerViewUserFeed.adapter = userFeedAdapter
-            recyclerViewUserFeed.layoutManager = LinearLayoutManager(requireContext())
-            recyclerViewUserFeed.visibility = View.VISIBLE
-
-            recyclerViewUserFavouritesFeed.adapter = userFavouriteFeedAdapter
-            recyclerViewUserFavouritesFeed.layoutManager = LinearLayoutManager(requireContext())
-            recyclerViewUserFavouritesFeed.visibility = View.GONE
+            textViewError.text = message
+            groupProfile.visibility = View.INVISIBLE
+            groupError.visibility = View.VISIBLE
+            progressBar.visibility = View.INVISIBLE
         }
     }
 
-    private fun startObserving() {
-        profileViewModel.userArticles.observe(viewLifecycleOwner) {
-            it?.let {
-                userFeedAdapter.submitData(viewLifecycleOwner.lifecycle, it)
-            }
-        }
-        profileViewModel.favouriteArticles.observe(viewLifecycleOwner) {
-            it?.let {
-                userFavouriteFeedAdapter.submitData(viewLifecycleOwner.lifecycle, it)
-            }
+    private fun setViewLoading() {
+        binding.apply {
+            groupProfile.visibility = View.INVISIBLE
+            groupError.visibility = View.INVISIBLE
+            progressBar.visibility = View.VISIBLE
         }
     }
 
-    inner class OnArticleClick() : ArticleAdapter.OnArticleClick {
-        override fun onItemClick(slug: String, articleType: ArticleType) {
-           val action =
-                ProfileFragmentDirections.actionProfileFragmentToArticleFragment(articleType, slug)
-            findNavController().navigate(action)
-        }
-
-        override fun onProfileClick(userName: String) {
-            val action = ProfileFragmentDirections.actionProfileFragmentSelf(userName)
-            findNavController().navigate(action)
+    private fun setViewProfile() {
+        binding.apply {
+            groupProfile.visibility = View.VISIBLE
+            groupError.visibility = View.INVISIBLE
+            progressBar.visibility = View.INVISIBLE
         }
     }
 
--- app/src/main/java/com/scaler/microblogs/viewmodels/ProfileViewModel.kt
+++ app/src/main/java/com/scaler/microblogs/viewmodels/ProfileViewModel.kt
@@ -4,16 +4,18 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.paging.PagingData
-import com.scaler.libconduit.models.Article
 import com.scaler.libconduit.models.Profile
+import com.scaler.libconduit.responses.ProfileResponse
 import com.scaler.microblogs.data.AppPrefStorage
 import com.scaler.microblogs.data.Repository
 import com.scaler.microblogs.di.AuthModule
+import com.scaler.microblogs.utils.NetworkResult
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
+import retrofit2.Response
 import javax.inject.Inject
 
 
@@ -22,8 +24,8 @@ class ProfileViewModel @Inject constructor(
     private val repo: Repository,
     private val appPrefStorage: AppPrefStorage
 ) : ViewModel() {
-    private val _profile = MutableLiveData<Profile>()
-    val profile: LiveData<Profile> = _profile
+    private val _profile = MutableLiveData<NetworkResult<Profile>>()
+    val profile: LiveData<NetworkResult<Profile>> = _profile
 
     private val _isLoggedIn = MutableLiveData<Boolean>()
     val isLoggedIn: LiveData<Boolean> = _isLoggedIn
@@ -31,78 +33,99 @@ class ProfileViewModel @Inject constructor(
     private val profileEventChannel = Channel<ProfileEvent>()
     val profileEvent = profileEventChannel.receiveAsFlow()
 
-    lateinit var userArticles: LiveData<PagingData<Article>>
-    lateinit var favouriteArticles: LiveData<PagingData<Article>>
-
-    private fun getUserToken(): Boolean {
+    fun checkIfLoggedIn() = viewModelScope.launch {
         val token = appPrefStorage.getUserToken()
-        return if (token.isNullOrEmpty()) {
-            false
+        if (token.isNullOrEmpty()) {
+            _isLoggedIn.postValue(false)
         } else {
             AuthModule.authToken = token
-            true
+            _isLoggedIn.postValue(true)
         }
     }
 
-
-    fun getUser(userName: String) = viewModelScope.launch {
-        getUserFavouriteArticle(userName)
-        getUserArticles(userName)
-        profileEventChannel.send(ProfileEvent.StartObserving)
-    }
-
-    fun getProfileFromRepo(userName: String) = viewModelScope.launch {
-        val result = repo.remote.getProfileByUserName(userName)
-        if (result.isSuccessful) {
-            _profile.postValue(result.body()?.profile!!)
+    fun getProfileFromRepo(userName: String) = viewModelScope.launch(Dispatchers.IO) {
+        _profile.postValue(NetworkResult.Loading())
+        try {
+            val result = repo.remote.getProfileByUserName(userName)
+            _profile.postValue(handleProfileResponse(result))
+        } catch (e: Exception) {
+            _profile.postValue(NetworkResult.Error(e.message.toString()))
         }
     }
 
     fun getProfileFromAuthRepo(userName: String) = viewModelScope.launch {
-        val result = repo.authRemote.getProfileByUserName(userName)
-        if (result.isSuccessful) {
-            _profile.postValue(result.body()?.profile!!)
+        _profile.postValue(NetworkResult.Loading())
+        try {
+            val result = repo.authRemote.getProfileByUserName(userName)
+            _profile.postValue(handleProfileResponse(result))
+        } catch (e: Exception) {
+            _profile.postValue(NetworkResult.Error(e.message.toString()))
         }
     }
 
-    private fun getUserFavouriteArticle(userName: String) {
-        //favouriteArticles = repo.remote.getFeedByUserFavourite(userName).cachedIn(viewModelScope)
-    }
-
-    private fun getUserArticles(userName: String) {
-       // userArticles = repo.remote.getFeedByUserName(userName).cachedIn(viewModelScope)
-    }
-
-    fun checkIfLoggedIn() = viewModelScope.launch {
-        if (getUserToken()) {
-            val result = repo.authRemote.getCurrentUser()
-            if (result.isSuccessful) {
-                _isLoggedIn.postValue(true)
-            } else {
-                _isLoggedIn.postValue(false)
+    private fun handleProfileResponse(response: Response<ProfileResponse>): NetworkResult<Profile> {
+        return when {
+            response.message().contains("timeout") -> {
+                NetworkResult.Error("Timeout")
             }
-        } else {
-            _isLoggedIn.postValue(false)
+            response.body()!!.profile == null -> {
+                NetworkResult.Error("User not Found")
+            }
+            response.isSuccessful -> {
+                val result = response.body()!!.profile!!
+                NetworkResult.Success(result)
+            }
+            else -> {
+                NetworkResult.Error(response.message())
+            }
         }
     }
 
-    fun followUnfollowAccount(userName: String, isFollowing: Boolean) = viewModelScope.launch {
+    fun followUnfollowAccount(userName: String, isFollowing: Boolean) {
         if (isFollowing) {
-            val result = repo.authRemote.unfollowAccount(userName)
-            if (result.isSuccessful) {
-                _profile.postValue(result.body()?.profile!!)
-            }
+            followAccount(userName)
         } else {
+            unFollowAccount(userName)
+        }
+    }
+
+    private fun followAccount(userName: String) = viewModelScope.launch(Dispatchers.IO) {
+        try {
             val result = repo.authRemote.followAccount(userName)
-            if (result.isSuccessful) {
-                _profile.postValue(result.body()?.profile!!)
+            handleFollowUnfollowResponse(result)
+        } catch (e: Exception) {
+            profileEventChannel.send(ProfileEvent.ErrorFollowUnFollow(e.message.toString()))
+        }
+    }
+
+    private fun unFollowAccount(userName: String) = viewModelScope.launch(Dispatchers.IO) {
+        try {
+            val result = repo.authRemote.unfollowAccount(userName)
+            handleFollowUnfollowResponse(result)
+        } catch (e: Exception) {
+            profileEventChannel.send(ProfileEvent.ErrorFollowUnFollow(e.message.toString()))
+        }
+    }
+
+    private suspend fun handleFollowUnfollowResponse(response: Response<ProfileResponse>) {
+        when {
+            response.message().contains("timeout") -> {
+                profileEventChannel.send(ProfileEvent.ErrorFollowUnFollow("timeout"))
+            }
+            response.body()!!.profile == null -> {
+                profileEventChannel.send(ProfileEvent.ErrorFollowUnFollow("something went wrong"))
+            }
+            response.isSuccessful -> {
+                val result = response.body()!!.profile!!
+                _profile.postValue(NetworkResult.Success(result))
+            }
+            else -> {
+                profileEventChannel.send(ProfileEvent.ErrorFollowUnFollow(response.message()))
             }
         }
     }
 
     sealed class ProfileEvent {
-        object StartObserving : ProfileEvent()
-        object LoggedOut : ProfileEvent()
-
+        data class ErrorFollowUnFollow(val errorMessage: String) : ProfileEvent()
     }
 }
