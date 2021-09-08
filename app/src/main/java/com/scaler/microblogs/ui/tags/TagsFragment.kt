aee4916d9fb79608a645aafc702b687e8226b420
Manish <manishjandu1@gmail.com>
2021-09-08 08:21:02 +0530

    FragmentTags:Internet and error handling setup ed

--- app/src/main/java/com/scaler/microblogs/ui/tags/TagsFragment.kt
+++ app/src/main/java/com/scaler/microblogs/ui/tags/TagsFragment.kt
@@ -1,51 +1,102 @@
 package com.scaler.microblogs.ui.tags
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.scaler.microblogs.R
 import com.scaler.microblogs.adapters.TagsAdapter
 import com.scaler.microblogs.databinding.FragmentTagsBinding
+import com.scaler.microblogs.utils.InternetConnectivity.ConnectivityManager
+import com.scaler.microblogs.utils.NetworkResult
 import com.scaler.microblogs.viewmodels.TagsViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
-class TagsFragment : Fragment() {
-
+class TagsFragment : Fragment(R.layout.fragment_tags) {
     private val tagsViewModel: TagsViewModel by viewModels()
     private var _binding: FragmentTagsBinding? = null
     private val binding get() = _binding!!
+    private val tagAdapter = TagsAdapter(OnTagClick())
 
-    companion object {
-        fun newInstance() = TagsFragment()
-    }
+    @Inject
+    lateinit var connectivityManager: ConnectivityManager
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentTagsBinding.inflate(inflater, container, false)
-        val tagAdapter = TagsAdapter(OnTagClick())
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentTagsBinding.bind(view)
 
-        tagsViewModel.getTags()
-
+        setViewLoading()
         binding.recyclerViewTags.apply {
             adapter = tagAdapter
             layoutManager = LinearLayoutManager(requireContext())
         }
+        observeInternetConnection()
+    }
 
-        tagsViewModel.tags.observe(viewLifecycleOwner) {
-            it?.let {
-                tagAdapter.submitList(it)
+    private fun observeInternetConnection() {
+        connectivityManager.isNetworkAvailable.observe(viewLifecycleOwner) {
+            it?.let { isInternetAvailable ->
+                if (isInternetAvailable) {
+                    observeTagsResponse()
+                } else {
+                    setViewError()
+                }
             }
         }
+    }
 
-        return binding.root
+    private fun observeTagsResponse() {
+        tagsViewModel.getTags()
+        tagsViewModel.tags.observe(viewLifecycleOwner) { response ->
+            when (response) {
+                is NetworkResult.Success -> {
+                    setViewTags()
+                    setDataInTags(response.data!!)
+                }
+                is NetworkResult.Error -> {
+                    setViewError(response.message.toString())
+                }
+                is NetworkResult.Loading -> {
+                    setViewLoading()
+                }
+            }
+        }
+    }
+
+    private fun setDataInTags(data: List<String>) {
+        tagAdapter.submitList(data)
+    }
+
+    private fun setViewLoading() {
+        binding.apply {
+            textViewError.visibility = View.INVISIBLE
+            imageViewError.visibility = View.INVISIBLE
+            recyclerViewTags.visibility = View.INVISIBLE
+            progressBar.visibility = View.VISIBLE
+        }
+    }
+
+    private fun setViewError(message: String = "No Internet Connection.") {
+        binding.apply {
+            textViewError.text = message
+            textViewError.visibility = View.VISIBLE
+            imageViewError.visibility = View.VISIBLE
+            recyclerViewTags.visibility = View.INVISIBLE
+            progressBar.visibility = View.INVISIBLE
+        }
+    }
+
+    private fun setViewTags() {
+        binding.apply {
+            textViewError.visibility = View.INVISIBLE
+            imageViewError.visibility = View.INVISIBLE
+            recyclerViewTags.visibility = View.VISIBLE
+            progressBar.visibility = View.INVISIBLE
+        }
     }
 
     inner class OnTagClick : TagsAdapter.OnItemTagClick {
--- app/src/main/java/com/scaler/microblogs/viewmodels/TagsViewModel.kt
+++ app/src/main/java/com/scaler/microblogs/viewmodels/TagsViewModel.kt
@@ -4,22 +4,47 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.scaler.libconduit.responses.TagsResponse
 import com.scaler.microblogs.data.Repository
+import com.scaler.microblogs.utils.NetworkResult
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import retrofit2.Response
 import javax.inject.Inject
 
 @HiltViewModel
 class TagsViewModel @Inject constructor(
     private val repo: Repository
 ) : ViewModel() {
-    private val _tags = MutableLiveData<List<String>>()
-    val tags: LiveData<List<String>> = _tags
+    private val _tags = MutableLiveData<NetworkResult<List<String>>>()
+    val tags: LiveData<NetworkResult<List<String>>> = _tags
 
-    fun getTags() = viewModelScope.launch {
-        val result = repo.remote.getTags()
-        result.body()!!.tags?.let {
-            _tags.postValue(it)
+    fun getTags() = viewModelScope.launch(Dispatchers.IO) {
+        _tags.postValue(NetworkResult.Loading())
+        try {
+            val result = repo.remote.getTags()
+            _tags.postValue(handleTagsResponse(result))
+        } catch (e: Exception) {
+            _tags.postValue(NetworkResult.Error(e.message.toString()))
+        }
+    }
+
+    private fun handleTagsResponse(response: Response<TagsResponse>): NetworkResult<List<String>>? {
+        return when{
+            response.message().contains("timeout") -> {
+                NetworkResult.Error("Timeout")
+            }
+            response.body()!!.tags == null -> {
+                NetworkResult.Error("tags not Found")
+            }
+            response.isSuccessful -> {
+                val result = response.body()!!.tags!!
+                NetworkResult.Success(result)
+            }
+            else -> {
+                NetworkResult.Error(response.message())
+            }
         }
     }
 
